447eb29adc691ae8a68a00aa1ff3a01bbe608897
SerlockWright <vohoangtin92@gmail.com>
2023-08-23 11:26:17 +0700

    feat/add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import './App.css'
+import Header from './components/UI/Header/Header'
 
 function App() {
   return (
     <React.Fragment>
+      <Header />
     </React.Fragment>
   )
 }
