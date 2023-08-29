82f6ff4b9f76f9967c8aab2b76102a81c36ec596
SerlockWright <vohoangtin92@gmail.com>
2023-08-29 15:49:57 +0700

    feat/handle add, remove item from cart, using reducer, context

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,25 +1,52 @@
 /* eslint-disable react/prop-types */
+import React from 'react';
 import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
+import CartContext from '../../context/CartContext';
+import CartItem from './CartItem';
 
 function Cart({ onClose }) {
-    const cartItem = (
-        <ul className={styles['cart-item']}>
-            {[{ id: 'c1', name: 'sushi', price: 12.99 }].map(item => (
-                <li key={item.id}>{item.name}</li>
+    const cartContext = React.useContext(CartContext);
+    const totalAmountCart = `$${cartContext.totalAmount.toFixed(2)}`;
+
+    const isItemExist = cartContext.items.length > 0;
+
+    const handleAddItem = (item) => {
+        cartContext.addItem({
+            ...item,
+            amount: 1
+        })
+    }
+
+    const handleRemoveItem = (id) => {
+        cartContext.removeItem(id)
+    }
+
+    const cartItems = (
+        <ul className={styles['cart-items']}>
+            {cartContext.items.map((item) => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={handleRemoveItem.bind(null, item.id)}
+                    onAdd={handleAddItem.bind(null, item)}
+                />
             ))}
         </ul>
-    )
+    );
+
     return (
         <Modal onClose={onClose}>
-            {cartItem}
+            {cartItems}
             <div className={styles.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmountCart}</span>
             </div>
             <div className={styles.action}>
                 <button className={styles['button--alt']} onClick={onClose}>Close</button>
-                <button className={styles.button}>Order</button>
+                {isItemExist && <button className={styles.button}>Order</button>}
             </div>
         </Modal>
     )
--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
+import React from "react";
+import styles from './Input.module.css';
 
-function Input({ label, input }) {
+const Input = React.forwardRef(({ label, input }, ref) => {
     return (
-        <div>
+        <div className={styles.input}>
             <label htmlFor={input.id}>{label}</label>
-            <input {...input} />
+            <input ref={ref} {...input} />
         </div>
     )
-}
+})
 
 export default Input
